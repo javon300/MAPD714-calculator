6f076ada024f780cde133a416d6efd690c85cf3e
matthew maxwell <matthewmaxwell300@yahoo.com>
2021-09-22 01:15:22 -0400

    first button completed

---
+++ MAPD714-calculator/MainViewControlller.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MAPD714-calculator
+//
+//  Created by javon maxwell on 2021-09-21.
+//
+
+import UIKit
+
+class MainViewControlller: UIViewController {
+    
+    //variable declarations
+    @IBOutlet weak var display: UITextField!
+    @IBOutlet weak var app_name_label: UILabel!
+    @IBOutlet weak var point_button: UIButton!
+    
+   
+    
+    //LifeCycle function
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func point_button_pressed(_ sender: UIButton) {
+          // self.display.text = "."
+       }
+
+}
+
