2f7e965df4e7c76beb559da6f74c6e4d30eb70d4
matthew maxwell <matthewmaxwell300@yahoo.com>
2021-10-01 15:58:17 -0400

    most buttons working except (. , %, sign change )

--- MAPD714-calculator/MainViewControlller.swift
+++ MAPD714-calculator/MainViewControlller.swift
@@ -1,28 +1,27 @@
-//
-//  ViewController.swift
-//  MAPD714-calculator
-//
-//  Created by javon maxwell on 2021-09-21.
-//
+/*
+ ViewController.swift
+ MAPD714-calculator
+ Author: Matthew Maxwell
+ id: 301200258
+ Created by matthew maxwell on 2021-09-21.
+*/
 
 import UIKit
 
 class MainViewControlller: UIViewController {
     
     //variable declarations
-    
+    var equation: String = ""
+    @IBOutlet weak var resultdisplay: UILabel!
     @IBOutlet weak var display: UILabel!
     //operations buttons/ yellow buttons
     @IBOutlet weak var point_button: UIButton!
- 
     @IBOutlet weak var divide_button: UIButton!
-    
     @IBOutlet weak var multiply_button: UIButton!
-    
-    
     @IBOutlet weak var subtract_button: UIButton!
     @IBOutlet weak var plus_button: UIButton!
     @IBOutlet weak var percent_button: UIButton!
+    
     @IBOutlet weak var sign_button: UIButton!
     @IBOutlet weak var clear_button: UIButton!
     @IBOutlet weak var equal_button: UIButton!
@@ -39,6 +38,80 @@ class MainViewControlller: UIViewController {
     @IBOutlet weak var nine_button: UIButton!
     @IBOutlet weak var zero_button: UIButton!
     
+    //number buttons action when pressed
+    @IBAction func zeroPressed(_ sender: UIButton){
+        appendEquation(value: "0")
+    }
+    @IBAction func onePressed(_ sender: UIButton) {
+        appendEquation(value:  "1")
+    }
+    @IBAction func twoPressed(_ sender: UIButton) {
+        appendEquation(value: "2")
+    }
+    @IBAction func threePressed(_ sender: UIButton) {
+        appendEquation(value:  "3")
+    }
+    @IBAction func fourPressed(_ sender: UIButton) {
+        appendEquation(value:  "4")
+    }
+    @IBAction func fivePressed(_ sender: UIButton) {
+        appendEquation(value:  "5")
+    }
+    @IBAction func sixPressed(_ sender: UIButton) {
+        appendEquation(value:  "6")
+    }
+    @IBAction func sevenPressed(_ sender: UIButton) {
+        appendEquation(value:  "7")
+    }
+    @IBAction func eightPressed(_ sender: UIButton) {
+        appendEquation(value:  "8")
+    }
+    @IBAction func ninePressed(_ sender: UIButton) {
+        appendEquation(value: "9")
+    }
+    
+    //operations buttons action when pressed
+    
+    @IBAction func plusPressed(_ sender: UIButton) {
+        appendEquation(value:  "+")
+    }
+    @IBAction func minusPressed(_ sender: UIButton) {
+        appendEquation(value:  "-")
+    }
+    @IBAction func multiplyPressed(_ sender: UIButton) {
+        appendEquation(value: "*")
+    }
+    @IBAction func dividePressed(_ sender: UIButton) {
+        appendEquation(value: "/")
+    }
+    @IBAction func modulus(_ sender: UIButton) {
+        appendEquation(value: "%")
+    }
+    @IBAction func signPressed(_ sender: UIButton) {
+        var sign = String("+")
+        if (sign == "+"){
+            sign = "-"
+            return
+        }
+        sign = "+"
+    }
+    @IBAction func pointPressed(_ sender: UIButton) {
+        appendEquation(value: ".")
+    }
+    @IBAction func clearPressed(_ sender: UIButton) {
+       clearAll()
+    }
+    @IBAction func equalPressed(_ sender: UIButton) {
+        let expression = NSExpression(format: equation)
+        let result = expression.expressionValue(with: nil, context: nil) as! Double
+        let resultString = formatResult(result: result)
+        resultdisplay.text = resultString
+        
+        
+    }
+    @IBAction func deletePressed(_ sender: UIButton) {
+        deleteLast()
+    }
     
     //LifeCycle function
     override func viewDidLoad() {
@@ -67,6 +140,29 @@ class MainViewControlller: UIViewController {
         eight_button.layer.cornerRadius = 15
         nine_button.layer.cornerRadius = 15
         zero_button.layer.cornerRadius = 15
+        clearAll()
+    }
+    
+    func clearAll(){
+        equation = ""
+        display.text = ""
+        resultdisplay.text = ""
+    }
+    func appendEquation(value: String){
+        equation = equation + value
+        display.text = equation
+    }
+    func deleteLast(){
+        equation.removeLast()
+        display.text?.removeLast()
+    }
+    func formatResult(result: Double) -> String{
+        if result.truncatingRemainder(dividingBy: 1) == 0{
+            return String(format: "%.0f",result)
+        }
+        else{
+            return String(format: "%.2f",result)
+        }
     }
 }
 
