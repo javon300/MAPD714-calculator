38de45f04712fa566a88e8bcc1252bbfa0f19d02
matthew maxwell <matthewmaxwell300@yahoo.com>
2021-10-24 22:29:07 -0600

    final commit
    a few of the new operands have incorrect calcs

--- MAPD714-calculator/MainViewControlller.swift
+++ MAPD714-calculator/MainViewControlller.swift
@@ -39,6 +39,14 @@ class MainViewControlller: UIViewController {
     @IBOutlet weak var nine_button: UIButton!
     @IBOutlet weak var zero_button: UIButton!
     @IBOutlet weak var point_button: UIButton!
+    //assignment part 3/ new buttons
+    @IBOutlet weak var sin_button: UIButton!
+    @IBOutlet weak var cos_button: UIButton!
+    @IBOutlet weak var tan_button: UIButton!
+    @IBOutlet weak var rand_button: UIButton!
+    @IBOutlet weak var pie_button: UIButton!
+    @IBOutlet weak var square_button: UIButton!
+    @IBOutlet weak var root_button: UIButton!
     
     //number buttons action when pressed
     @IBAction func zeroPressed(_ sender: UIButton){
@@ -91,12 +99,33 @@ class MainViewControlller: UIViewController {
     @IBAction func modulus(_ sender: UIButton) {
         appendEquation(value: " % ")
     }
+    @IBAction func sinPressed(_ sender: UIButton) {
+        appendEquation(value: " sin ")
+    }
+    @IBAction func cosPressed(_ sender: UIButton) {
+        appendEquation(value: " cos ")
+    }
+    @IBAction func tanPressed(_ sender: UIButton) {
+        appendEquation(value: " tan ")
+    }
+    @IBAction func squarePressed(_ sender: UIButton) {
+        appendEquation(value: " ² ")
+    }
+    @IBAction func randPressed(_ sender: UIButton) {
+        appendEquation(value: " ran ")
+    }
+    @IBAction func rootPressed(_ sender: UIButton) {
+        appendEquation(value: " √ ")
+    }
+    @IBAction func piePressed(_ sender: UIButton) {
+        appendEquation(value: " π ")
+    }
     
     @IBAction func signPressed(_ sender: UIButton) {
        signChange()
     }
     
-    
+
     @IBAction func clearPressed(_ sender: UIButton) {
        clearAll()
     }
@@ -106,6 +135,8 @@ class MainViewControlller: UIViewController {
     @IBAction func deletePressed(_ sender: UIButton) {
         deleteLast()
     }
+    //part three new operands button action when pressed
+    
     
     //LifeCycle function
     override func viewDidLoad() {
@@ -133,9 +164,17 @@ class MainViewControlller: UIViewController {
         eight_button.layer.cornerRadius = 15
         nine_button.layer.cornerRadius = 15
         zero_button.layer.cornerRadius = 15
+        // part 3 new operands
+        sin_button.layer.cornerRadius = 15
+        cos_button.layer.cornerRadius = 15
+        tan_button.layer.cornerRadius = 15
+        square_button.layer.cornerRadius = 15
+        pie_button.layer.cornerRadius = 15
+        rand_button.layer.cornerRadius = 15
+        rand_button.layer.cornerRadius = 15
         clearAll()
     }
-    //calears all values from all containers
+    //clears all values from all containers
     func clearAll(){
         equation = ""
         display.text = ""
@@ -143,15 +182,25 @@ class MainViewControlller: UIViewController {
     }
     
     //places values after theu are pressed
-    func appendEquation(value: String){
+    func appendEquation(value: String)
+    {
         equation = equation + value
         display.text = equation
     }
     
-    func deleteLast(){
+    //places values after theu are pressed
+    func appendResult(value: String)
+    {
+        equation = equation + value
+        resultdisplay.text = value
+    }
+    
+    func deleteLast()
+    {
         equation.removeLast()
         display.text?.removeLast()
     }
+    
     //add remove the negation sign
     func signChange()
     {
@@ -201,7 +250,7 @@ class MainViewControlller: UIViewController {
             return String(format: "%.0f",result)
         }
         else{
-            return String(format: "%.2f",result)
+            return String(format: "%.8f",result)
         }
     }
     func Calculation()
@@ -209,8 +258,42 @@ class MainViewControlller: UIViewController {
         let hold = equation.split(separator: " ")
         print(hold);print( String(hold.count) )
         
+        if(hold.count == 2)
+        {
+            var1 = String(hold[0])
+            var2 = String(hold[1])
+            // choose operator
+            if (hold[0] == "sin")
+            {
+                trig(operand: var1, num: var2)
+            }
+            else if (hold[0] == "cos")
+            {
+                trig(operand: var1, num: var2)
+            }else if (hold[0] == "tan")
+            {
+                trig(operand: var1, num: var2)
+            }
+            else if (hold[0] == "ran")
+            {
+                rand(operand: var1, num2: var2)
+            }
+            else if (hold[0] == "²")
+            {
+                square(operand:var1, num1: var2)
+            }
+            else if (hold[0] == "√")
+            {
+                root(operand: var1, num: var2)
+            }
+            else
+            {
+                print("no trig values")
+            }
+            
+        }
         //check if array count is odd, if even then not valid equation
-        if (hold.count % 2 == 1)
+        else if (hold.count % 2 == 1)
         {
             //when given two operands and one operator
             if (hold.count == 3)
@@ -242,22 +325,16 @@ class MainViewControlller: UIViewController {
                 {
                     print("invalid operator, please enter operator.")
                 }
-            }
-            else if(hold.count == 3)
-            {
-                
-            }
-            
-        }
+            }//basic operands
+        }// mod check on variables
         else
         {
             print("invalid equation")
             return
         }
         
-        //check for multiplication
-        
-    }
+    }//calculation close
+
     func addition(num1: String, num2: String)
     {
         var res: Double!
@@ -268,8 +345,10 @@ class MainViewControlller: UIViewController {
         val2 = Double(num2)
         res = val1 + val2
         print("res" + String(res))
+        appendResult(value: formatResult(result: res))
         
     }
+    
     func modulus(num1: String, num2: String)
     {
         var res: Int!
@@ -280,8 +359,10 @@ class MainViewControlller: UIViewController {
         val2 = Int(num2)
         res = val1 % val2
         print("res" + String(res))
+        resultdisplay.text = String(res)
         
     }
+    
     func subtraction(num1: String, num2: String)
     {
         var res: Double!
@@ -292,8 +373,10 @@ class MainViewControlller: UIViewController {
         val2 = Double(num2)
         res = val1 - val2
         print("res" + String(res))
+        appendResult(value: formatResult(result: res))
         
     }
+    
     func multiplication(num1: String, num2: String)
     {
         var res: Double!
@@ -304,8 +387,10 @@ class MainViewControlller: UIViewController {
         val2 = Double(num2)
         res = val1 * val2
         print("res" + String(res))
+        appendResult(value: formatResult(result: res))
         
     }
+    
     func division(num1: String, num2: String)
     {
         var res: Double!
@@ -316,6 +401,75 @@ class MainViewControlller: UIViewController {
         val2 = Double(num2)
         res = val1 / val2
         print("res" + String(res))
+        appendResult(value: formatResult(result: res))
+        
+    }
+    
+    func trig(operand: String, num: String)
+    {
+        var res: Double!
+        var val1: Double!
+        
+
+        val1 = Double(num)
+        
+        if (operand == "sin")
+        {
+            res = sin(val1)
+        }
+        else if (operand == "cos")
+        {
+            res = cos(val1)
+        }
+        else if (operand == "tan")
+        {
+            res = tan(val1)
+        }
+        else
+        {
+            res = 0.00
+        }
+        print("res" + String(res))
+        appendResult(value: formatResult(result: res))
+
+    }
+    func root(operand: String, num: String)
+    {
+        var res: Double!
+        var val1: Double!
+        
+        val1 = Double(num)
+        res = val1 / 2
+        print("res" + String(res))
+        appendResult(value: formatResult(result: res))
+        
+    }
+    func pie(num1: String, num2: String)
+    {
+        var res: Double!
+        
+        res = 3.14
+        print("res" + String(res))
+        appendResult(value: formatResult(result: res))
+        
+    }
+    func rand(operand: String, num2: String)
+    {
+        var res: Double!
+        res = 0.13
+        print("res" + String(res))
+        appendResult(value: formatResult(result: res))
+        
+    }
+    func square(operand: String, num1: String)
+    {
+        var res: Double!
+        var val1: Double!
+        
+        val1 = Double(num1)
+        res = val1 * val1
+        print("res" + String(res) + String(operand))
+        appendResult(value: formatResult(result: res))
         
     }
 }
